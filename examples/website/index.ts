ec08a295e45d14ee7273ec838aaf30e10863a388
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2021-07-24 23:44:22 +0300

    Update example

--- examples/website/index.ts
+++ examples/website/index.ts
@@ -9,6 +9,8 @@ import {
 
 const root = document.getElementById("root");
 
+const LOADING_TIME = 500;
+
 type Session = {
   token: string | null;
   setToken: (token: string) => void;
@@ -95,7 +97,7 @@ const LoginForm = () => {
     setTimeout(() => {
       setLoading(false);
       session.setToken("1234");
-    }, 500);
+    }, LOADING_TIME);
   };
 
   return c(
@@ -105,7 +107,7 @@ const LoginForm = () => {
       "form",
       {
         onSubmit,
-        class: `w-96 relative ${loading ? "opacity-60" : ""}`,
+        class: `w-96 relative mt-10 ${loading ? "opacity-60" : ""}`,
       },
       loading
         ? c(
@@ -117,6 +119,7 @@ const LoginForm = () => {
             c(Spinner)
           )
         : null,
+      c("h1", { class: "text-xl font-bold mb-3 text-gray-700" }, "Sign in"),
       c(Label, { for: "email" }, "Email"),
       c(Input, {
         email,
@@ -135,13 +138,17 @@ const LoginForm = () => {
         loading,
         placeholder: "********",
       }),
-      c(Button, { loading }, "Sign in")
-    ),
-    c(
-      "div",
-      { class: "flex justify-center text-sm mt-4 text-gray-400" },
-      "Copyright 2021 ",
-      c("a", { href: "cool.company", class: "text-blue-500" }, "cool.company")
+      c(Button, { loading }, "Sign in"),
+      c(
+        "div",
+        { class: "flex justify-center text-sm mt-4 text-gray-400" },
+        "Copyright © 2021",
+        c(
+          "a",
+          { href: "https://example.org", class: "text-blue-500 ml-1" },
+          "example.org"
+        )
+      )
     )
   );
 };
@@ -155,14 +162,14 @@ type PostData = {
 const Post = ({ author, content, avatarColor }: PostData) => {
   return c(
     "div",
-    { class: "border border-gray-200 rounded-md p-4 mb-4" },
+    { class: "p-4" },
     c(
       "div",
       { class: "flex space-x-3" },
       c("div", { class: `rounded-full ${avatarColor} h-12 w-12` }),
       c(
         "div",
-        { class: "flex-1 py-1" },
+        { class: "flex-1" },
         c("div", { class: "font-bold text-gray-700" }, author),
         c("div", {}, c("div", { class: "text-gray-500" }, content))
       )
@@ -173,20 +180,19 @@ const Post = ({ author, content, avatarColor }: PostData) => {
 const PlaceholderPost = () => {
   return c(
     "div",
-    { class: "border border-gray-200 rounded-md p-4 mb-4" },
+    { class: "p-4" },
     c(
       "div",
-      { class: "animate-pulse flex space-x-3" },
+      { class: "flex space-x-3" },
       c("div", { class: "rounded-full bg-gray-200 h-12 w-12" }),
       c(
         "div",
         { class: "flex-1 space-y-3 py-1" },
-        c("div", { class: "h-4 bg-gray-200 rounded w-3/4" }),
+        c("div", { class: "h-3 bg-gray-200 rounded w-1/4" }),
         c(
           "div",
           { class: "space-y-2" },
-          c("div", { class: "h-4 bg-gray-200 rounded" }),
-          c("div", { class: "h-4 bg-gray-200 rounded w-5/6" })
+          c("div", { class: "h-3 bg-gray-200 rounded w-3/4" })
         )
       )
     )
@@ -201,28 +207,55 @@ const Page = () => {
     setTimeout(() => {
       setData([
         {
-          author: "tchayen1",
-          avatarColor: "bg-blue-400",
-          content: "123",
+          author: "Alice",
+          avatarColor: "bg-red-400",
+          content: "Setting up my twitter",
         },
         {
-          author: "test",
+          author: "Bob",
+          avatarColor: "bg-yellow-500",
+          content: "Anyone wants to hang out some time later today?",
+        },
+        {
+          author: "Charlie",
+          avatarColor: "bg-yellow-300",
+          content: "Twitter profile balloons day it is!",
+        },
+        {
+          author: "Dylan",
           avatarColor: "bg-green-400",
-          content: "Hello",
+          content:
+            "I woke up and had coffee. Can't wait to start eating a breakfast. And then lunch. And then, who knows, maybe I will have a dinner, maybe I will skip eating for the rest of the day, maybe I will prepare food for the morning.",
+        },
+        {
+          author: "Ethan",
+          avatarColor: "bg-blue-400",
+          content: "Lorem ipsum dolor sit amet",
+        },
+        {
+          author: "Franklin",
+          avatarColor: "bg-purple-400",
+          content: "I just had a good sandwich",
         },
       ]);
       setLoading(false);
-    }, 500);
+    }, LOADING_TIME * 3);
   }, []);
 
   return c(
     "div",
-    { class: "max-w-sm w-full mx-auto" },
+    { class: "mx-auto border-l border-r", style: { width: "600px" } },
     loading
-      ? c("div", {}, c(PlaceholderPost), c(PlaceholderPost), c(PlaceholderPost))
+      ? c(
+          "div",
+          { class: "divide-y" },
+          c(PlaceholderPost),
+          c(PlaceholderPost),
+          c(PlaceholderPost)
+        )
       : c(
           "div",
-          {},
+          { class: "divide-y" },
           data.map((post) => c(Post, post))
         )
   );
@@ -236,7 +269,7 @@ const App = () => {
     { value: { token, setToken } },
     c(
       "div",
-      { class: "w-screen h-screen flex justify-center p-10" },
+      { class: "w-screen h-screen flex justify-center" },
       token ? c(Page) : c(LoginForm)
     )
   );
