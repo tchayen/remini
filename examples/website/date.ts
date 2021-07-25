238ef52644938e462129f2dca027b71e2541c240
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2021-07-25 15:34:45 +0300

    Improve example

---
+++ examples/website/date.ts
@@ -0,0 +1,39 @@
+const SECOND = 1000;
+const MINUTE = 60 * SECOND;
+const HOUR = 60 * MINUTE;
+const DAY = 24 * HOUR;
+const MONTH = 30 * DAY;
+const YEAR = 12 * MONTH;
+
+const format = (ms: number, abbreviation: string, unit: number) => {
+  const amount = Math.floor(ms / unit);
+
+  return `${amount}${abbreviation}`;
+};
+
+export const getFriendlyTime = (date: Date) => {
+  const ms = new Date() - date;
+  if (isNaN(ms) || !isFinite(ms)) {
+    throw new Error(`Wrong value provided: ${ms}`);
+  }
+
+  if (ms < SECOND) {
+    return "second ago";
+  } else if (ms < MINUTE) {
+    const amount = Math.floor(ms / SECOND);
+    if (amount < 2) {
+      return `second ago`;
+    }
+    return `few seconds ago`;
+  } else if (ms < HOUR) {
+    return format(ms, "m", MINUTE);
+  } else if (ms < DAY) {
+    return format(ms, "h", HOUR);
+  } else if (ms < MONTH) {
+    return format(ms, "d", DAY);
+  } else if (ms < YEAR) {
+    return format(ms, "m", MONTH);
+  } else {
+    return format(ms, "y", YEAR);
+  }
+};
--- examples/website/index.ts
+++ examples/website/index.ts
@@ -6,16 +6,125 @@ import {
   useEffect,
   useState,
 } from "../../lib";
+import { getFriendlyTime } from "./date";
 
 const root = document.getElementById("root");
 
-const LOADING_TIME = 500;
+const LOADING_TIME = 300;
 
 type Session = {
   token: string | null;
   setToken: (token: string) => void;
 };
+
 const SessionContext = createContext<Session>();
+
+type User = {
+  fullName: string;
+  login: string;
+  avatar: string;
+  following: number;
+  followers: number;
+  bio: string;
+};
+
+type Post = {
+  author: number;
+  content: string;
+  timestamp: number;
+};
+
+const TEXT_PRIMARY = "text-gray-700";
+const TEXT_SECONDARY = "text-gray-500";
+
+const users: User[] = [
+  {
+    fullName: "Alice",
+    login: "alice",
+    avatar: "bg-red-400",
+    following: 10,
+    followers: 456,
+    bio: "React Native developer at some company. I like trains.",
+  },
+  {
+    fullName: "Bob",
+    login: "bob",
+    avatar: "bg-yellow-500",
+    following: 10,
+    followers: 456,
+    bio: "React Native developer at some company. I like trains.",
+  },
+  {
+    fullName: "Charlie",
+    login: "charlie",
+    avatar: "bg-yellow-300",
+    following: 10,
+    followers: 456,
+    bio: "React Native developer at some company. I like trains.",
+  },
+  {
+    fullName: "Dylan",
+    login: "dylan",
+    avatar: "bg-green-400",
+    following: 10,
+    followers: 456,
+    bio: "React Native developer at some company. I like trains.",
+  },
+  {
+    fullName: "Ethan",
+    login: "ethan",
+    avatar: "bg-blue-400",
+    following: 10,
+    followers: 456,
+    bio: "React Native developer at some company. I like trains.",
+  },
+  {
+    fullName: "Franklin",
+    login: "franklin",
+    avatar: "bg-purple-400",
+    following: 10,
+    followers: 456,
+    bio: "React Native developer at some company. I like trains.",
+  },
+];
+
+const posts: Post[] = [
+  {
+    author: 0,
+    content: "Setting up my twitter",
+    timestamp: 1627207002,
+  },
+  {
+    author: 1,
+    content: "Anyone wants to hang out some time later today?",
+    timestamp: 1627210082,
+  },
+  {
+    author: 2,
+    content: "Twitter profile balloons day it is!",
+    timestamp: 1627202300,
+  },
+  {
+    author: 3,
+    content:
+      "I woke up and had coffee. Can't wait to start eating a breakfast. And then lunch. And then, who knows, maybe I will have a dinner, maybe I will skip eating for the rest of the day, maybe I will prepare food for the morning.",
+    timestamp: 1627201733,
+  },
+  {
+    author: 4,
+    content: "Lorem ipsum dolor sit amet",
+    timestamp: 1627202798,
+  },
+  {
+    author: 5,
+    content: "I just had a good sandwich",
+    timestamp: 1627214404,
+  },
+];
+
+const Avatar = ({ author }: { author: number }) => {
+  return c("div", { class: `rounded-full ${users[author].avatar} h-12 w-12` });
+};
 
 const Input = ({ loading, ...props }: any) => {
   return c(
@@ -24,8 +133,7 @@ const Input = ({ loading, ...props }: any) => {
     c("input", {
       ...props,
       ...(loading ? { disabled: "disabled" } : {}),
-      class:
-        "p-2 bg-white appearance-none border border-gray-200 rounded w-full text-gray-700 leading-tight focus:outline-none focus:ring focus:border-blue-500",
+      class: `p-2 bg-white appearance-none border border-gray-200 rounded w-full ${TEXT_PRIMARY} leading-tight focus:outline-none focus:ring focus:border-blue-500`,
     })
   );
 };
@@ -33,7 +141,7 @@ const Input = ({ loading, ...props }: any) => {
 const Label = ({ children, ...props }: any) => {
   return c(
     "label",
-    { ...props, class: "text-sm text-gray-700 font-bold" },
+    { ...props, class: `text-sm ${TEXT_PRIMARY} font-bold` },
     children
   );
 };
@@ -109,7 +217,7 @@ const LoginForm = () => {
         onSubmit,
         class: `w-96 relative mt-10 ${loading ? "opacity-60" : ""}`,
       },
-      c("h1", { class: "text-xl font-bold mb-3 text-gray-700" }, "Sign in"),
+      c("h1", { class: `text-xl font-bold mb-3 ${TEXT_PRIMARY}` }, "Sign in"),
       c(Label, { for: "email" }, "Email"),
       c(Input, {
         email,
@@ -131,7 +239,7 @@ const LoginForm = () => {
       c(Button, { loading }, "Sign in"),
       c(
         "div",
-        { class: "flex justify-center text-sm mt-4 text-gray-400" },
+        { class: `flex justify-center text-sm mt-4 ${TEXT_SECONDARY}` },
         "Copyright © 2021",
         c(
           "a",
@@ -153,7 +261,7 @@ const LoginForm = () => {
   );
 };
 
-const Author = ({ author }: { author: string }) => {
+const Author = ({ author }: { author: number }) => {
   const [show, setShow] = useState(false);
 
   const onMouseOver = () => {
@@ -170,44 +278,102 @@ const Author = ({ author }: { author: string }) => {
     show
       ? c(
           "div",
-          { class: "absolute top-4 bg-white p-3 rounded shadow" },
-          c("div", {}, author)
+          {
+            class:
+              "absolute top-6 bg-white p-4 rounded border border-gray-200 shadow-lg z-10",
+          },
+          c(
+            "div",
+            {},
+            c(
+              "div",
+              { class: "flex justify-between" },
+              c(Avatar, { author }),
+              c(
+                "div",
+                {
+                  class:
+                    "h-9 px-4 flex justify-center items-center border border-gray-200 rounded-full font-bold text-gray-400 cursor-pointer",
+                },
+                "Follow"
+              )
+            ),
+            c(
+              "div",
+              { class: `${TEXT_PRIMARY} font-bold` },
+              users[author].fullName
+            ),
+            c("div", { class: `${TEXT_SECONDARY}` }, `@${users[author].login}`),
+            c("div", { class: `${TEXT_SECONDARY} my-2` }, users[author].bio),
+            c(
+              "div",
+              { class: `flex space-x-3 ${TEXT_SECONDARY}` },
+              c(
+                "div",
+                { class: "flex" },
+                `Following:`,
+                c(
+                  "div",
+                  { class: `ml-1 font-bold ${TEXT_PRIMARY}` },
+                  `${users[author].following}`
+                )
+              ),
+              c(
+                "div",
+                { class: "flex" },
+                `Followers:`,
+                c(
+                  "div",
+                  { class: `ml-1 font-bold ${TEXT_PRIMARY}` },
+                  `${users[author].followers}`
+                )
+              )
+            )
+          )
         )
       : null,
     c(
       "a",
       {
-        class: "font-bold text-gray-700 hover:underline",
+        class: `font-bold ${TEXT_PRIMARY} hover:underline`,
         href: `/${author}`,
         onMouseOver,
         onMouseOut,
       },
-      author
+      users[author].fullName
     )
   );
 };
 
 type PostData = {
-  author: string;
+  author: number;
   content: string;
-  avatarColor: string;
+  timestamp: number;
 };
 
-const Post = ({ author, content, avatarColor }: PostData) => {
+const Post = ({ author, content, timestamp }: PostData) => {
+  const time = getFriendlyTime(new Date(timestamp * 1000));
   return c(
     "div",
     { class: "p-4" },
     c(
       "div",
       { class: "flex space-x-3" },
-      c("div", {
-        class: `rounded-full ${avatarColor} h-12 w-12`,
-      }),
+      c(Avatar, { author }),
       c(
         "div",
         { class: "flex-1" },
-        c(Author, { author }),
-        c("div", {}, c("div", { class: "text-gray-500" }, content))
+        c(
+          "div",
+          { class: "flex" },
+          c(Author, { author }),
+          c(
+            "div",
+            { class: `ml-1 ${TEXT_SECONDARY}` },
+            `@${users[author].login} · ${time}`
+          )
+        ),
+        c("div", {}, c("div", { class: `${TEXT_SECONDARY}` }, content))
       )
     )
   );
@@ -224,7 +390,12 @@ const PlaceholderPost = () => {
       c(
         "div",
         { class: "flex-1 space-y-3 py-1" },
-        c("div", { class: "h-3 bg-gray-200 rounded w-1/4" }),
+        c(
+          "div",
+          { class: "flex space-x-1" },
+          c("div", { class: "h-3 bg-gray-200 rounded w-1/5" }),
+          c("div", { class: "h-3 bg-gray-200 rounded w-1/6" })
+        ),
         c(
           "div",
           { class: "space-y-2" },
@@ -241,41 +412,9 @@ const Page = () => {
 
   useEffect(() => {
     setTimeout(() => {
-      setData([
-        {
-          author: "Alice",
-          avatarColor: "bg-red-400",
-          content: "Setting up my twitter",
-        },
-        {
-          author: "Bob",
-          avatarColor: "bg-yellow-500",
-          content: "Anyone wants to hang out some time later today?",
-        },
-        {
-          author: "Charlie",
-          avatarColor: "bg-yellow-300",
-          content: "Twitter profile balloons day it is!",
-        },
-        {
-          author: "Dylan",
-          avatarColor: "bg-green-400",
-          content:
-            "I woke up and had coffee. Can't wait to start eating a breakfast. And then lunch. And then, who knows, maybe I will have a dinner, maybe I will skip eating for the rest of the day, maybe I will prepare food for the morning.",
-        },
-        {
-          author: "Ethan",
-          avatarColor: "bg-blue-400",
-          content: "Lorem ipsum dolor sit amet",
-        },
-        {
-          author: "Franklin",
-          avatarColor: "bg-purple-400",
-          content: "I just had a good sandwich",
-        },
-      ]);
+      setData(posts);
       setLoading(false);
-    }, LOADING_TIME * 3);
+    }, LOADING_TIME);
   }, []);
 
   return c(
@@ -299,8 +438,6 @@ const Page = () => {
 
 const App = () => {
   const [token, setToken] = useState<string | null>(null);
-
-  console.log(token);
 
   return c(
     SessionContext.Provider,
